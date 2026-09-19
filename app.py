441f7aec268334b4bcb77f3c3db18c5ea00f732d
evcgitorg <44972972+evcgitorg@users.noreply.github.com>
2026-09-19 11:21:37 -0500

    Add sidebar image and title to Streamlit app

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import streamlit as st
 
 st.title("Paradigmas de la programación")
 
+st.sidebar.image(UCGlogo.png)
+
 st.sidebar.title("Parámetros")
 
 st.write("Elaborado por Enrique Villalta")
+
+
