c4496732e8c85bba58f1e8d1cb3ec6de8507dd6c
evcgitorg <44972972+evcgitorg@users.noreply.github.com>
2026-09-19 11:32:24 -0500

    Update input defaults and pass parameters to function

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ st.sidebar.title("Parámetros")
 
 st.write("Elaborado por Enrique Villalta")
 
-capital = st.number_input("Ingrese el capital: ")
-tasa_anual_pct = st.number_input("Ingrese tasa anual: ")
-dias_mora = st.number_input("Ingrse los días de mora: ")
+capital = st.number_input("Ingrese el capital: ", value = 1000)
+tasa_anual_pct = st.number_input("Ingrese tasa anual: ", value = 0.15)
+dias_mora = st.number_input("Ingrse los días de mora: ", value = 30)
 
-resultado = lf.calcular_interes_mora()
+resultado = lf.calcular_interes_mora(capital,tasa_anual_pct,dias_mora)
 
-
+st.write("El resultado por atraso es: ",resultado)
