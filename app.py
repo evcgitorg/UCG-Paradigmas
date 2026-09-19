536521fcab2ce6e3c6f07a6db550a3d516bec232
evcgitorg <44972972+evcgitorg@users.noreply.github.com>
2026-09-19 11:29:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import libreria_funciones as lf
+
+
 
 st.title("Paradigmas de la programación")
 
@@ -7,5 +10,11 @@ st.sidebar.image("UCGlogo.png")
 st.sidebar.title("Parámetros")
 
 st.write("Elaborado por Enrique Villalta")
+
+capital = st.number_input("Ingrese el capital: ")
+tasa_anual_pct = st.number_input("Ingrese tasa anual: ")
+dias_mora = st.number_input("Ingrse los días de mora: ")
+
+resultado = lf.calcular_interes_mora()
 
 
